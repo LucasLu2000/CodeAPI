76f39c7eec35ff1d89d998f5198cd06c358e90b6
LucasLu2000 <LucasLu2000@github.com>
2022-07-19 17:49:31 -0400

    Added functions

--- hammingCode.h
+++ hammingCode.h
@@ -5,8 +5,8 @@
 class HammingCode {
     public:
         HammingCode();
-        Row<int> GolayEncode(Row<int> the_word);
-        Row<int> GolayDecode(Row<int> the_receivedWord); // this function finds the original word
+        Row<int> HammingEncode(Row<int> the_word);
+        Row<int> HammingDecode(Row<int> the_receivedWord); // this function finds the original word
 
     private:
         Mat<int> parityCheck(int n, int r, int q);
