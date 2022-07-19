266937f48e05c23554892da40598bd84415e29bf
LucasLu2000 <LucasLu2000@github.com>
2022-07-19 10:12:23 -0400

    Added a few functions

--- codeBasic.h
+++ codeBasic.h
@@ -1,12 +1,26 @@
 #ifndef CODEBASIC_H
 #define CODEBASIC_H
+#include <iostream>
+#include <fstream>
+#include <armadillo>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
 
 using namespace std;
+using namespace arma;
 
 class Code {
-public:
+    public:
+        Code(Row<int>);
+        Mat<int> randomMatrix (int r, int c, int q);
+        int compareRowVec (Row<int> a, Row<int> b);
+        Mat<int> nonNegativeMatrix (Mat<int> M, int q);
+        Mat<int> dividesMatrix (Mat<int> M, int x, int q);
+        
 
-private:
+    private:
 
 };
 
