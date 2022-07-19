b3395173bdd34df5ff6eef1560660d04ee406bb8
LucasLu2000 <LucasLu2000@github.com>
2022-07-19 17:30:51 -0400

    Added functions

--- hammingCode.cpp
+++ hammingCode.cpp
@@ -1,1 +1,11 @@
 #include "hammingCode.h"
+
+Mat<int> HammingCode::randomMatrix(int r, int c, int q) {
+    Mat<int> M(r,c,fill::zeros);
+    for (int i=0; i<r; i++) {
+        for (int j=0; j<c; j++) {
+            M(i,j) = rand() % q;
+        }
+    }
+    return M;
+}
